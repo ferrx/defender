46b36265323b9276969fd6eb459e8b722c4c0393
Steve Ferrell <sferrell@hq.prizum.com>
2018-02-18 10:22:57 -0500

    Cleaned up index and controllers for pre-lambda serverless approach

--- src/client/app/main/main.controller.js
+++ src/client/app/main/main.controller.js
@@ -1,27 +1,27 @@
 'use strict';
 
 angular.module('defenderApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
+    // $http.get('/api/things').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    //   socket.syncUpdates('thing', $scope.awesomeThings);
+    // });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
+    $scope.addThing = function () {
+      if ($scope.newThing === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
+      // $http.post('/api/things', { name: $scope.newThing });
       $scope.newThing = '';
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.deleteThing = function (thing) {
+      // $http.delete('/api/things/' + thing._id);
     };
 
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
+    // $scope.$on('$destroy', function () {
+    //   socket.unsyncUpdates('thing');
+    // });
   });
