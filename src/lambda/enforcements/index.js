733efd6864111c92a7466f68784665105a5be448
Steve Ferrell <sferrell@hq.prizum.com>
2018-03-04 14:40:03 -0500

    Modified lambda backend to limit to 180 days, added api key

--- src/lambda/enforcements/index.js
+++ src/lambda/enforcements/index.js
@@ -1,21 +1,40 @@
 const request = require('request');
 
+var formatDate = function (date) {
+  var yyyy = date.getFullYear();
+  var mm = date.getMonth() < 9 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+  var dd = date.getDate() < 10 ? "0" + (date.getDate()) : date.getDate();
+  return yyyy.toString() + mm.toString() + dd.toString();
+}
+
 exports.handler = (event, context, callback) => {
-  let params = event['pathParameters'];
   let queryStrings = event['queryStringParameters'];
 
-  if (params.callType === 'food') {
-    request.get('https://api.fda.gov/food/enforcement.json?limit=10', function (error, response, body) {
+  //get today - 90 days
+  let currentDate = new Date();
+  let formattedCurDate = formatDate(currentDate);
+  currentDate.setDate(currentDate.getDate() - 180);
+  let formattedPrevDate = formatDate(currentDate);
 
-      var response = {
-        "statusCode": 200,
-        "headers": {
-          "my_header": "my_value"
-        },
-        "body": JSON.stringify(body),
-        "isBase64Encoded": false
-      };
-      callback(null, response);
-    });
+  let queryString = "?api_key=97hexPQBqiRG7qeNL5LCubmalvKuWQIhCjnrOHLB&limit=100";
+  queryString += "&search=report_date:[" + formattedPrevDate + "+TO+" + formattedCurDate + "]";
+
+  if (queryStrings && queryStrings.search) {
+    queryString += "+AND+(" + queryStrings.search + ")";
   }
+
+  let apiUrl = 'https://api.fda.gov/' + queryStrings.type + '/enforcement.json';
+  apiUrl += queryString;
+
+  request.get(apiUrl, function (error, response, body) {
+    var response = {
+      "statusCode": 200,
+      "headers": {
+        "Access-Control-Allow-Origin": "*"
+      },
+      "body": JSON.stringify(body),
+      "isBase64Encoded": false
+    };
+    callback(null, response);
+  });
 };
